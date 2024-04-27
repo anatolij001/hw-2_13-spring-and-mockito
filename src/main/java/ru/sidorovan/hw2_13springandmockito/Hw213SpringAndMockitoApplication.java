060ee2583e2b030423d73b59f5fbc547dec9ba42
anatolij001 <anatolij001@gmail.com>
2024-04-27 16:35:15 +0300

    Initial commit.

---
+++ src/main/java/ru/sidorovan/hw2_13springandmockito/Hw213SpringAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package ru.sidorovan.hw2_13springandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw213SpringAndMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw213SpringAndMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sidorovan/hw2_13springandmockito/Hw213SpringAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sidorovan.hw2_13springandmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw213SpringAndMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
